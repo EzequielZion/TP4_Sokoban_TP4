420ba6562942411c9ffbaf0884a8550def3a942d
SantiRavaglia <santi23062000@gmail.com>
2020-12-07 19:49:20 -0300

    direccionnnn

--- src/Mapa.cpp
+++ src/Mapa.cpp
@@ -1,91 +1,34 @@
 #include "Mapa.h"
 
 
-Mapa::Mapa() : _paredes(), _depositos() {
-    // los constructores por defecto de vector tiene complejidad constante.
-    // O(1)
-}
-
-Mapa::Mapa(const Mapa &aCopiar) {
-    *this = aCopiar; // O(P + D)
-}
+Mapa::Mapa() : _paredes(), _depositos() { }
 
 Mapa &Mapa::operator=(const Mapa &aCopiar) {
-    // copiar un vector tiene complejidad lineal
-    _paredes = aCopiar._paredes;    // O(P)
-    _depositos = aCopiar._depositos; // O(D)
 
-    return *this;
 }
 
 bool Mapa::AgPared(Coord p) {
-    bool res = !_BuscarOrdenado(_depositos, p); // O(log D)
-    if(res) { // si en p no hay un depósito, es posible agregar la pared
-        res = _InsertarOrdenado(_paredes, p); // O(P)
-    }
-    return res;
+
 }
 
 bool Mapa::AgDeposito(Coord d) {
-    bool res = !_BuscarOrdenado(_paredes, d);   // O(log P)
-    if(res) { // si en p no hay una pared, es posible agregar el depósito
-        res = _InsertarOrdenado(_depositos, d); // o(D)
-    }
-    return res;
+
 }
 
 bool Mapa::HayPared(Coord c) const {
-    return _BuscarOrdenado(_paredes, c); // O(log P)
+
 }
 
 bool Mapa::HayDeposito(Coord c) const {
-    return _BuscarOrdenado(_depositos, c); // O(log D)
+
 }
 
 set<Coord> Mapa::Depositos() const {
-    set<Coord> ret = set<Coord>(); // O(1)
-    for (int i = 0; i < _depositos.size(); ++i) {
-        ret.insert(_depositos[i]); // O(log i)
-    } // O(n * log n)
-    return ret;
+
 }
 
-bool Mapa::_InsertarOrdenado(vector<Coord> &vec, Coord c) {
-    bool res = !_BuscarOrdenado(vec, c); // O(log |vec|)
-    if(res) {
-        vec.push_back(c); // O(|vec|) con reallocation
-        int i = vec.size() - 1;
-        while (i > 0 && vec[i] < vec[i - 1]) { // O(|vec|)
-            // swap
-            Coord tmp = vec[i];
-            vec[i] = vec[i - 1];
-            vec[i - 1] = tmp;
+set<Coord> Mapa::Paredes() const {
 
-            i--;
-        }
-    }
-
-    return res;
-} // O(|vec| + log |vec|) = O(|vec|)
-
-bool Mapa::_BuscarOrdenado(const vector<Coord> &vec, Coord c) const {
-    if(vec.size() == 0 || c < vec[0] || c > vec[vec.size() - 1]) { // O(1)
-        return false;
-    } else if(c == vec[vec.size() - 1]) { // O(1)
-        return true;
-    }
-    // else
-    // busqueda binaria, O(log |vec|)
-    int izq = 0;
-    int der = vec.size() - 1;
-    while (izq + 1 < der && c != vec[izq]) {
-        int mid = (izq + der) /2;
-        if(c < vec[mid]) {
-            der = mid;
-        } else {
-            izq = mid;
-        }
-    }
-
-    return c == vec[izq];
 }
+
+set<Coord>
--- src/Mapa.h
+++ src/Mapa.h
@@ -7,79 +7,30 @@
 
 class Mapa {
 public:
-
-    /**
-     * Constructor
-     * Complejidad O(1)
-     */
     Mapa();
 
-    /**
-     * Constructor por copia
-     * Complejidad O(D + P)
-     */
-    Mapa(const Mapa& aCopiar);
-
-    /**
-     * Operador asignación
-     * Complejidad O(D + P)
-     */
     Mapa& operator=(const Mapa& aCopiar);
 
-    /**
-     * Agrega una pared en la coordenada indicada
-     * si no hay ni un depósito ni otra pared en dicha coordenada.
-     * Devuelve true si la pared se agregó.
-     * Complejidad: O(P + log D)
-     */
     bool AgPared(Coord p);
 
-    /**
-     * Agrega un depósito en la coordenada indicada
-     * si no hay ni otro depósito ni una pared en dicha coordenada.
-     * Devuelve true si el depósito se agregó.
-     * Complejidad: O(D + log P)
-     */
     bool AgDeposito(Coord d);
 
-    /**
-     * Devuelve true si hay una pared en la coordenada indicada.
-     * Complejidad O(log P)
-     */
     bool HayPared(Coord c) const;
 
-    /**
-     * Devuelve true si hay un depósito en la coordenada indicada.
-     * Complejidad O(log D)
-     */
     bool HayDeposito(Coord c) const;
 
-    /**
-     * Devuelve el conjunto de depósitos del mapa.
-     * Complejidad O(D * log D)
-     */
     set<Coord> Depositos() const;
+
+    set<Coord> Paredes() const;
+
+    set<Coord> bombasTiradas() const;
 
 private:
     vector<pair<int,  vector<int>>> _paredes;
     vector<pair<int,  vector<int>>> _depositos;
     list<Coordenada> _bombasTiradas;
-
-    /**
-     * Inserta la coordenada c ordenadamente en el vector
-     * si no estaba.
-     * Devuelve true si la coordenada c no estaba en vec
-     * y se pudo insertar.
-     * Complejidad O(|vec|)
-     */
-    bool _InsertarOrdenado(vector<Coord>& vec, Coord c);
-
-    /**
-     * Devuelve true si la cordenada c está en vec.
-     * Complejidad O(log |vec|)
-     */
-    bool _BuscarOrdenado(const vector<Coord>& vec, Coord c) const;
 };
 
+using namespace std;
 
 #endif //TP4_SOKOBAN_MAPA_H
