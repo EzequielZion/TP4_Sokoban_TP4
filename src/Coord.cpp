feb2eea49fc981429f4e7b5f9b4f01f439f3c038
SantiRavaglia <santi23062000@gmail.com>
2020-12-07 18:39:15 -0300

    coordenada ready

--- src/Coord.cpp
+++ src/Coord.cpp
@@ -8,4 +8,24 @@ Coord::Coord(int X, int Y) : _coord(X, Y) {}
 
 Coord::Coord(Coordenada &coord) : _coord(coord) {}
 
+int Coord::x() const {
+    return this->_coord.first;
+}
 
+int Coord::y() const {
+    return this->_coord.second;
+}
+
+bool Coord::operator!=(const Coord &coord) {
+    return this->_coord.first != coord.x() || this->_coord.second != coord.y();
+}
+
+bool Coord::operator==(const Coord &coord) {
+    return this->_coord.first == coord.x() && this->_coord.second == coord.y();
+}
+
+bool Coord::operator=(const Coord &coord) {
+    this->_coord.first = coord.x();
+    this->_coord.second = coord.y();
+    return this;
+}
