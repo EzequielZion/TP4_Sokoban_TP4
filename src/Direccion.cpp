81da213eb2b5a7309cec16ff253c7d3ad4b0be6e
SantiRavaglia <santi23062000@gmail.com>
2020-12-07 19:08:42 -0300

    direccionnnn

--- src/Direccion.cpp
+++ src/Direccion.cpp
@@ -5,5 +5,16 @@
 #include "Direccion.h"
 #include "Coord.h"
 
+Direccion::Direccion(PuntoCardinal pc) {
+    if (pc = Norte) {
+        _ord = 0;
+    } else if (pc = Este) {
+        _ord = 1;
+    } else if (pc = Sur) {
+        _ord = 2;
+    } else if (pc = Oeste) {
+        _ord = 3;
+    }
 
+}
 
